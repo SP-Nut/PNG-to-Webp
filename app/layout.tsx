095856c3b1475e680c8f0a3e9926c99d5013c3e9
SP-Nut <spnutaomi@gmail.com>
2025-07-22 11:12:06 +0700

    Add Google AdSense integration for monetization

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="th">
+      <head>
+        <script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2812903229696322"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
